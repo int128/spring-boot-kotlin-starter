fd5f2cf92adabbb77b65c5dc5f4e9d767b06a421
Nils Schmidt <nils.schmidt.ger@gmail.com>
2016-12-17 13:55:34 +0100

    Scan only application related packages for spring beans
    It is not necessary to scan all(!) packages. Plus, this avoids 'org.hibernate.HibernateException: Access to DialectResolutionInfo cannot be null' which was introduced with dependency updates of spring-boot.

--- src/main/kotlin/org/hidetake/app/App.kt
+++ src/main/kotlin/org/hidetake/app/App.kt
@@ -3,7 +3,7 @@ package org.hidetake.app
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = arrayOf("org.hidetake.app"))
 open class App {
     companion object {
         @JvmStatic
